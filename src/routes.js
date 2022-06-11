7f7300deb342e6a80ce8fc9b7f180a3c5a507338
Dimitar Sergev <102225734+DimitarSergeev@users.noreply.github.com>
2022-06-11 18:35:05 +0300

    render Login & Register Page
    Create controller

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ const experss = require('express');
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/authController')
 
 const router = experss.Router();
 
 router.use('/', homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use('/auth', userController)
 
 module.exports = router;
