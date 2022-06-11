f9a449db768eb03b059d7ace4320db947741a3b5
Dimitar Sergev <102225734+DimitarSergeev@users.noreply.github.com>
2022-06-11 18:55:50 +0300

    Create register functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService')
 
-router.get('/login',(req,res)=>{
-      res.render('auth/login')
+
+router.get('/login', (req, res) => {
+    res.render('auth/login')
 })
 
-router.get('/register',(req,res)=>{
-     res.render('auth/register')
+// router.post('/login',(req,res)=>{
+
+// })
+
+
+router.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
+router.post('/register', async (req, res) => {
+    const user = await authService.register(req.body)
+    if (user) {
+        res.redirect('/auth/login')
+    }else{
+        res.redirect('/404')
+    }
+
+
 })
 
 module.exports = router
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,20 @@
- const User = require('../models/user')
 
- 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user')
+
+const saltRound = 10
+
+exports.register = async ({username,password,repeatPassword})=>{
+         if (password !== repeatPassword) {
+             return false
+         }
+
+         let hashedPass = await bcrypt.hash(password,saltRound)
+
+         return User.create({
+             username,
+             password: hashedPass
+         })
+}
